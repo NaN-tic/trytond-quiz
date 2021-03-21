17ff114d28cae3aacd4976cc68f9c255cc1ad674
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-21 11:50:33 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .quiz import *
+from . import quiz
 
 def register():
     Pool.register(
-        QuizCategory,
-        QuizQuestion,
-        QuizQuestionOption,
-        Quiz,
-        QuizOption,
+        quiz.QuizCategory,
+        quiz.QuizQuestion,
+        quiz.QuizQuestionOption,
+        quiz.Quiz,
+        quiz.QuizOption,
         module='quiz', type_='model')
